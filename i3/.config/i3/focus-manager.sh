a39981a3671c6f4a49646da971d614a9ba789550
Juan Corona <juan.carlos.corona.r@gmail.com>
2018-09-27 22:18:35 -0700

    Simplify focus manager script

--- i3/.config/i3/focus-manager.sh
+++ i3/.config/i3/focus-manager.sh
@@ -8,17 +8,11 @@ xprop -root -spy _NET_ACTIVE_WINDOW | while read win; do
         Y=$((Y + (HEIGHT / 2)))
 
         xdotool mousemove ${X} ${Y}
-        
+        sleep 0.1
         jetbrainsPID=`xdotool search --onlyvisible --class 'jetbrains-' getwindowpid`
         windowPID=`xdotool getactivewindow getwindowpid`
         if [ "$jetbrainsPID" == "$windowPID" ]; then
-        	xdotool getactivewindow windowactivate
-        	xdotool getactivewindow windowfocus
-        	xdotool getactivewindow key Shift Shift Escape
+        	xdotool key Shift Shift Escape
         fi
-        unclutter --timeout 0 &
-        last_pid=$!
-        sleep 0.1
-        kill -KILL $last_pid
     fi
 done
